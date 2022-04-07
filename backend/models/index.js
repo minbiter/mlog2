8f12d3f455ce42ee8e15ec52e54bd0ba78eba35a
Chanwoo Min <cksdn135@kookmin.ac.kr>
2022-04-07 19:57:46 +0900

    refactor: from mysql2 to mysql2/promise (#4)

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,45 +1,40 @@
 require("dotenv").config();
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 const User = require("./user");
 const Diary = require("./diary");
 const Sentiment = require("./sentiment");
+const Music = require("./music");
 
 const env = process.env.NODE_ENV || "DEVELOPMENT";
 
-const config = () => {
+const config = async () => {
   // mysql connect
-  const connection = mysql.createConnection({
+  const connection = await mysql.createConnection({
     host: "localhost",
     user: "root",
     password: process.env[`${env}_DB_PASSWORD`],
   });
-  // connection success||failure eventHandler
-  connection.connect((err) => {
-    if (err) {
-      throw err;
-    } else {
-      console.log("(step1) 연결 성공");
-      console.log("(step2-1) SCHEMA 생성시작");
-      // create mlog database
-      connection.query(
-        "CREATE SCHEMA IF NOT EXISTS mlog DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci",
-        (err) => {
-          if (err) throw err;
-          console.log("(step2-2) SCHEMA 생성 완료");
-          console.log("(step3) tables 생성 시작");
-          createTable(connection);
-        }
-      );
-    }
-  });
+  // connection success
+  console.log("(step1) 연결 성공");
+  console.log("(step2-1) SCHEMA 생성시작");
+  // create mlog database
+  await connection.query(
+    "CREATE SCHEMA IF NOT EXISTS mlog DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci"
+  );
+  console.log("(step2-2) SCHEMA 생성 완료");
+  console.log("(step3) tables 생성 시작");
+  await createTable(connection);
+
   // create tables
-  function createTable(connection) {
-    User.init(connection);
+  async function createTable(connection) {
+    await User.init(connection);
     console.log("(step3-1) user table 생성 완료");
-    Diary.init(connection);
+    await Diary.init(connection);
     console.log("(step3-2) diary table 생성 완료");
-    Sentiment.init(connection);
+    await Sentiment.init(connection);
     console.log("(step3-3) sentiment table 생성 완료");
+    await Music.init(connection);
+    console.log("(step3-4) music table 생성 완료");
   }
 };
 
